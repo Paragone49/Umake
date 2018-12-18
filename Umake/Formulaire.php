c383b324222659fca57f47b5f39403f0ca81424f
Paragone49 <45964011+Paragone49@users.noreply.github.com>
2018-12-18 10:29:07 +0100

    Update Formulaire.php

--- Umake/Formulaire.php
+++ Umake/Formulaire.php
@@ -6,14 +6,19 @@
 
 <body>
 <form id="form1" name="form1" method="post" action="formulaire-traitement.php">
-  <p>
-    <label for="textfield">Animal</label>
-    <input type="text" name="Animal" id="Animal" autocomplete="on"/>
-  </p>
+<!--  <p>-->
+<!--    <label for="textfield">Animal</label>-->
+<!--    <input type="text" name="Animal" id="Animal" autocomplete="on"/>-->
+<!--  </p>-->
+    <select name="Animal">
+        <option value="lapin">Lapin</option>
+        <option value="loup">Loup</option>
+        <option value="ours">Ours</option>
+    </select>
     <fieldset id="Qualité"><b>Qualité</b><br>
         <input type="radio" value="etoile1" name="etoile">Une étoile<br>
         <input type="radio" value="etoile2" name="etoile">Deux étoiles<br>
-        <input type="radio" value="etoime3" name="etoile">Trois étoiles<br>
+        <input type="radio" value="etoile3" name="etoile">Trois étoiles<br>
     </fieldset>
     <input type="submit" name="button" id="button" value="Envoyer" />
   </p>
