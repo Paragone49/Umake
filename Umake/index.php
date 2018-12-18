31a90c3ea0f2269bf612e95ef3c7a12224ad98d2
Arthur8639 <45964205+Arthur8639@users.noreply.github.com>
2018-12-18 10:50:25 +0100

    Add Ours et Loup
    Pas les bonnes valeurs de prix

--- Umake/index.php
+++ Umake/index.php
@@ -1,1 +1,51 @@
 <?php
+
+
+
+
+
+$Dictionnaire=array(
+    $loup = array(
+        $etoile1= array( $viande = array("GrosGibier",1,0.75),
+            $peau = array("PeauDeLoup",1,4),
+            $carcasse = array("CarcasseLoup",1,5),
+            $loot = array("CoeurDeLoup",1,2),
+        ),
+        $etoile2= array( $viande = array("GrosGibier",2,0.75),
+            $peau = array("PeauDeLoup",1,5),
+            $carcasse = array("CarcasseLoup",1,5),
+            $loot = array("CoeurDeLoup",1,2),
+        ),
+        $etoile3= array( $viande = array("GrosGibier",4,0.75),
+            $peau = array("PeauDeLoup",1,5),
+            $carcasse = array("CarcasseLoup",1,5),
+            $loot = array("CoeurDeLoup",1,2),
+        ),
+    ),
+    $ours = array(
+        $etoile1= array( $viande = array("GrosGibier",1,0.75),
+            $peau = array("PeauDeOurs",1,5),
+            $loot = array("GriffeDeOurs",1,2),
+        ),
+        $etoile2= array( $viande = array("GrosGibier",2,0.75),
+            $peau = array("PeauDeLoup",1,6),
+            $loot = array("GriffeDeOurs",1,2),
+        ),
+        $etoile3= array( $viande = array("GrosGibier",4,0.75),
+            $peau = array("PeauDeLoup",1,7),
+            $loot = array("GriffeDeOurs",1,2),
+        ),
+    ),
+);
+
+
+
+
+foreach ($loup as $raret){
+    foreach ($raret as $loot){
+        foreach ($loot as $desk){
+            echo " ".$desk;
+        }
+    }
+}
+
