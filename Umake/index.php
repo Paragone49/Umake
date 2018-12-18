274faf71ac7a88ffc441b2e556ceec91b8ac9951
Arthur8639 <45964205+Arthur8639@users.noreply.github.com>
2018-12-18 11:26:13 +0100

    Update index.php

--- Umake/index.php
+++ Umake/index.php
@@ -5,7 +5,7 @@
 
 
 $Dictionnaire=array(
-    $loup = array(
+    $loup = array("loup",
         $etoile1= array( $viande = array("GrosGibier",1,0.75),
             $peau = array("PeauDeLoup",1,4),
             $carcasse = array("CarcasseLoup",1,5),
@@ -22,30 +22,22 @@ $Dictionnaire=array(
             $loot = array("CoeurDeLoup",1,2),
         ),
     ),
-    $ours = array(
+    $ours = array("Ours",
         $etoile1= array( $viande = array("GrosGibier",1,0.75),
             $peau = array("PeauDeOurs",1,5),
             $loot = array("GriffeDeOurs",1,2),
         ),
         $etoile2= array( $viande = array("GrosGibier",2,0.75),
-            $peau = array("PeauDeLoup",1,6),
+            $peau = array("PeauDeOurs",1,6),
             $loot = array("GriffeDeOurs",1,2),
         ),
         $etoile3= array( $viande = array("GrosGibier",4,0.75),
-            $peau = array("PeauDeLoup",1,7),
+            $peau = array("PeauDeOurs",1,7),
             $loot = array("GriffeDeOurs",1,2),
         ),
     ),
 );
-
-
-
-
-foreach ($loup as $raret){
-    foreach ($raret as $loot){
-        foreach ($loot as $desk){
-            echo " ".$desk;
-        }
-    }
+foreach ($Dictionnaire as $Animal){
+    echo $Animal[0];
 }
 
